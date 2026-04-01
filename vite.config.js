6007b4ba642e440bc45c28c4d39a80b0324545ae
刘1300 <8641121+liu-1300@user.noreply.gitee.com>
2026-04-01 09:29:15 +0800

    fort后

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'test',  // 指定打包输出目录为 test，而不是默认的 dist
+  }
 })
